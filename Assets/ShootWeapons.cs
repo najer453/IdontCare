12b15bb8545c954b9dc376d384abd840e46892a7
kaylee <morga395@minidokaschools.org>
2019-05-01 10:57:15 -0600

    upd

---
+++ Assets/ShootWeapons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWeapons : MonoBehaviour
+{/*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWeapons : MonoBehaviour {
+
+    public GameObject barrel;
+    public LaserSiteControll IzrSight;
+    public flaot speed;
+    public float angle;
+
+
+    public UFOController
+    */
+}
