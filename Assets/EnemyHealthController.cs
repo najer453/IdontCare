a4d4d756b23f69c8289c0505d2bdf48917c8d3f6
yesenia <najer453@minidokaschools.org>
2019-05-14 11:07:32 -0600

    update

---
+++ Assets/EnemyHealthController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthController : MonoBehaviour
+{
+
+    public int health;
+    //Use this for initialization
+    void Start()
+    { 
+
+    }
+//Update is called once per frame 
+void Update() 
+{
+    if (health <= 0)
+    {
+        gameObject.transform.parent.gameObject.SetActive(false);
+        //gameObject.SetActive(false):
+        }
+
+}
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+        }
+}
+
+
---
+++ Assets/MoveBullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public Rigidbody2D myRB2D;
+    public int bulletDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB2D.velocity = transform.right * movementSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            //do nothing
+        }
+        else if (other.tag == "Enemy")
+        {
+            //do damage
+            other.GetComponent<EnemyHealthController>().TakeDamage(bulletDamage);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
